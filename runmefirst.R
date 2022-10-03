a5be4d95bb7b2b3df499b80f6b7e080dff5dc977
James Durant <hzd3@cdc.gov>
2022-10-03 15:40:49 -0400

    updating with more information.

--- runmefirst.R
+++ runmefirst.R
@@ -6,32 +6,47 @@ library(tidyverse)
 orcid_auth()
 
 
-edu <- rorcid::orcid_educations("0000-0002-3378-7386")
-
-edux <- detailed_entries(edu$`0000-0002-3378-7386`$`education-summary`, what =`role-title`,
-                 when=as.character(glue::glue("{`start-date.year.value`} - {`end-date.year.value`}")),
-                 with = organization.name,
-                 where = glue::glue("{`organization.address.city`}, {`organization.address.region`}")
+orcid_edu <- do.call("rbind",
+                      rorcid::orcid_educations("0000-0002-3378-7386")$`0000-0002-3378-7386`$`affiliation-group`$summaries
 )
+
+
+
+edux <- orcid_edu %>%
+  detailed_entries(
+    what = `education-summary.role-title`,
+    when = glue::glue("{`education-summary.start-date.year.value`} - {`education-summary.end-date.year.value`}"),
+    with = `education-summary.organization.name`,
+    where = glue::glue("{`education-summary.organization.address.city`}, {`education-summary.organization.address.region`}") 
+  )
+
+
+
 
 
 
 mycvdata <- list(edux = edux)
 
 
-emp <- orcid_employments("0000-0002-3378-7386")
+emp <- rorcid::orcid_employments("0000-0002-3378-7386")$`0000-0002-3378-7386`$`affiliation-group`$summaries %>%
+  bind_rows()
+
 
 # emp$`0000-0002-3378-7386`$`employment-summary`$`role-title` <- 
 #   paste(emp$`0000-0002-3378-7386`$`employment-summary`$`role-title`, "\\hfill \\break")
 # 
 
-emp$`0000-0002-3378-7386`$`employment-summary`$`end-date.year.value`[is.na(emp$`0000-0002-3378-7386`$`employment-summary`$`end-date.year.value`)] <- "     Present"
+emp$`employment-summary.end-date.year.value`[is.na(emp$`employment-summary.end-date.year.value`)] <- "     Present"
 
-empx <- detailed_entries(emp$`0000-0002-3378-7386`$`employment-summary`, what =`role-title`,
-                         when=as.character(glue::glue("{`start-date.year.value`} - {`end-date.year.value`}")),
-                         with = organization.name,
-                         where = glue::glue("{`organization.address.city`}, {`organization.address.region`}"),
-                         .protect = FALSE)
+empx <- emp %>%
+  detailed_entries(
+    what = `employment-summary.role-title`,
+    when = glue::glue("{`employment-summary.start-date.year.value`} - {`employment-summary.end-date.year.value`}"),
+    with = `employment-summary.organization.name`,
+    where = glue::glue("{`employment-summary.organization.address.city`},{`employment-summary.organization.address.region`}")
+  )
+
+
 
 mycvdata$empx <- empx
 pubs <- works(orcid_id("0000-0002-3378-7386"))
@@ -51,7 +66,10 @@ mycvdata$pubsx <- detailed_entries(mycvdata$pubsx,
     when = year,
     with = author,
     where = journal
-  )
+  ) %>%
+  filter(where != "Geol Soc Am Abstr Program")
+
+
 
 
 pres <- read_excel("Data.xlsx", sheet = "Short Presentations")
@@ -61,6 +79,17 @@ pres <- read_excel("Data.xlsx", sheet = "Short Presentations")
 pres <- pres[order(strptime(pres$Start.Date, format = "%m/%d/%Y"), decreasing = TRUE),]
 pres <- pres[order(pres$Start.Date, decreasing = TRUE),]
 pres$where <- paste(pres$Sponsor, pres$Location)
-mycvdata$presx <- detailed_entries(pres, what = Title,  when = Start.Date, where=where, with=Authors)
+mycvdata$presx <- detailed_entries(pres, what = Title,  where = where, when = Start.Date,  with=Authors)
+
+
+
+govt <- read_excel("Data.xlsx", sheet = "Governmental Publications")
+
+govt <- govt[order(strptime(govt$Start.Date, format = "%m/%d/%Y"), decreasing = TRUE),]
+govt <- govt[order(govt$Start.Date, decreasing = TRUE),]
+govt$where <- paste(govt$Sponsor, govt$Location)
+mycvdata$govtx <- detailed_entries(govt, what = Title,  when = format(Start.Date, "%Y"), where=where, with=Authors)
+
+
 
 write_rds(mycvdata, "./mycvdata.rds")
